097771c7ffe87004d7760b331e70c7613084a196
Dom Rodriguez <shymega@shymega.org.uk>
2020-08-02 22:23:16 +0100

    [guile-rs]: Run `rustfmt` on `src/lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -18,34 +18,40 @@
 extern crate guile_sys;
 extern crate libc;
 
-use std::ffi;
 use libc::{c_char, c_void};
+use std::ffi;
 
-// use libc::c_void;
-
-pub struct GuileVM {
-}
+pub struct GuileVM {}
 
 pub fn init<F>(func: F)
-    where F: Fn(GuileVM) {
+where
+    F: Fn(GuileVM),
+{
     unsafe {
-        guile_sys::scm_with_guile(Some(with_guile_callback::<F>), &func as *const _ as *mut c_void);
+        guile_sys::scm_with_guile(
+            Some(with_guile_callback::<F>),
+            &func as *const _ as *mut c_void,
+        );
     }
 }
 
 impl GuileVM {
     pub fn shell(&self, args: Vec<String>) {
         unsafe {
-            let mut argv: Vec<*mut c_char> = args.into_iter().map(|arg| {
-                ffi::CString::new(arg).unwrap().into_raw()
-            }).collect();
+            let mut argv: Vec<*mut c_char> = args
+                .into_iter()
+                .map(|arg| ffi::CString::new(arg).unwrap().into_raw())
+                .collect();
             let argv_ptr = argv.as_mut_ptr();
             guile_sys::scm_shell(argv.len() as i32, argv_ptr);
         }
     }
 }
 
-unsafe extern fn with_guile_callback<F>(data: *mut c_void) -> *mut c_void where F: Fn(GuileVM) {
+unsafe extern "C" fn with_guile_callback<F>(data: *mut c_void) -> *mut c_void
+where
+    F: Fn(GuileVM),
+{
     let callback = data as *mut F;
 
     let vm = GuileVM {};
